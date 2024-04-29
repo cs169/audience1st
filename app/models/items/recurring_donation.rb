baa275de728eaa99d75169452ab456389ccfa3ee
rnickles <robbynickles@gmail.com>
2024-04-28 21:14:02 -0700

    changed syntax

--- app/models/items/recurring_donation.rb
+++ app/models/items/recurring_donation.rb
@@ -6,22 +6,24 @@ class RecurringDonation < Item
 
   def one_line_description ; end
   def description_for_audit_txn ; end
+  def first_donation
+    # Retrieves the earliest donation based on sold_on
+    Donation.where(recurring_donation_id: id).order(sold_on: :asc).first
+  end
   def monthly_amount
     # Finds the first donation with the recurring_donation_id matching this instance's id
-    donation = Donation.find_by(recurring_donation_id: id)
+    donation = first_donation
     donation ? donation.amount : nil  # Returns the donation amount if found, otherwise returns nil
-  end
-  def total_to_date
-    RecurringDonation.find(id).donations.sum(:amount)
   end
   def item_description
     # Finds the first donation with the recurring_donation_id matching this instance's id
-    donation = Donation.find_by(recurring_donation_id: id)
+    donation = first_donation
     donation ? donation.item_description : nil # Returns the donation item description if found, otherwise returns nil
   end
-  def first_donation
-    # Retrieves the earliest donation based on the created_at timestamp
-    Donation.where(recurring_donation_id: id).order(sold_on: :asc).first
+  def total_to_date
+    # Calculate the sum of all donations belonging to this recurring donation
+    Donation.where(recurring_donation_id: id).sum(:amount)
+
   end
   def start_date
     first_donation.sold_on
